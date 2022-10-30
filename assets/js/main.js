8a3ae407ba42acf72a4009fe19620f41a5a9339a
miradonis <miradonis@web.de>
2022-10-30 22:35:43 +0100

    changed files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,125 +1,78 @@
-console.log("test");
 
-function plusVat() {
+
+function plus_minusVat(event) {
+  event.preventDefault();
+
+  let calculation;
   /**
    *  prüfen ob mwst dazugerechnet werden soll
    */
 
-  if (document.getElementById("netto_brutto").checked) {
-    document.getElementById("output").innerHTML =
-      "Nettobetrag (Preis ohne Mehrwertsteuer) in Euro";
-    document.getElementById("brutto_netto_value").innerHTML =
-      "Bruttobetrag (Endpreis)";
-
-    /**
-     * prüfen ob 19% ausgewählt wurde
-     */
-    if (document.getElementById("nineteen_percent").checked) {
-      const userInput = Number(document.getElementById("userInput").value);
-      console.log(userInput);
+  if (document.getElementById("netto_brutto").checked) { 
+    document.getElementById("output").innerHTML = "Nettobetrag (Preis ohne Mehrwertsteuer) in Euro";
+    document.getElementById("brutto_netto_value").innerHTML = "Bruttobetrag (Endpreis)";
 
       /**
-       *  berechnung mwst 19%
+       * -prüfen ob 19% ausgewählt wurde
+       * -berechnung
+       * -daten zurück ins html
        */
-      const calculation = userInput * 0.19;
-      console.log(calculation);
+      if (document.getElementById("nineteen_percent").checked) {
+        const userInput = Number(document.getElementById("userInput").value);
 
-      /**
-       *  mwst zurück ins html
-       * */
-      document.getElementById("mwst").innerHTML =
-        Math.round(calculation * 100) / 100;
+        calculation = userInput * 0.19;
 
-      /**
-       *  gesamtbetrag zurück ins html
-       */
-      document.getElementById("final_price").innerHTML =
-        Math.round((userInput + calculation) * 100) / 100;
+        document.getElementById("mwst").innerHTML = Math.round(userInput * 0.19) + " €";
+        document.getElementById("final_price").innerHTML = Math.round((userInput + calculation) * 100) / 100 + " €";
 
-      /**
-       * prüfen ob 7% ausgewählt wurde
-       */
-    } else if (document.getElementById("seven_percent").checked) {
-      const userInput = Number(document.getElementById("userInput").value);
-      console.log(userInput);
+        /**
+         * -7% wurde ausgewählt
+         * -berechnung
+         * -daten zurück ins html
+         */
+      } else {
+        const userInput = Number(document.getElementById("userInput").value);
 
-      /**
-       *  berechnung mwst 7%
-       */
-      const calculation = userInput * 0.07;
-      console.log(calculation);
+        calculation = userInput * 0.07;
 
-      /**
-       * mwst zurück ins html
-       */
+        document.getElementById("mwst").innerHTML = Math.round(calculation) + " €";
+        document.getElementById("final_price").innerHTML = Math.round(userInput + calculation) + " €";
+      }
 
-      document.getElementById("mwst").innerHTML = Math.round(calculation);
-
-      /**
-       * gesamtbetrag zurück ins html
-       */
-      document.getElementById("final_price").innerHTML = Math.round(
-        userInput + calculation
-      );
-    }
-  } else if (document.getElementById("brutto_netto").checked) {
     /**
      * prüfen ob mwst abgezogen werden soll
      */
-    document.getElementById("output").innerHTML =
-      "Bruttobetrag (Preis inklusive Mehrwertsteuer) in Euro";
+  } else {
     document.getElementById("brutto_netto_value").innerHTML = "Nettobetrag";
-
-    /**
-     * prüfen ob 19% ausgewählt wurde
-     */
-
-    if (document.getElementById("nineteen_percent").checked) {
-      const userInput = Number(document.getElementById("userInput").value);
-      console.log(userInput);
+    document.getElementById("output").innerHTML = "Bruttobetrag (Preis inklusive Mehrwertsteuer) in Euro";
+  
 
       /**
-       *  berechnung mwst 19%
-       */
-      const calculation = userInput * 0.19;
-      console.log(calculation);
-
-      /**
-       *  mwst zurück ins html
-       */
-      document.getElementById("mwst").innerHTML =
-        Math.round(calculation * 100) / 100;
-
-      /**
-       *  gesamtbetrag zurück ins html
-       */
-      document.getElementById("final_price").innerHTML =
-        Math.round((userInput - calculation) * 100) / 100;
-
-      /**
-       * prüfen ob 7% ausgewählt wurde
-       */
-    } else if (document.getElementById("seven_percent").checked) {
-      const userInput = Number(document.getElementById("userInput").value);
-      console.log(userInput);
-
-      /**
-       *  berechnung mwst 7%
-       */
-      const calculation = userInput * 0.07;
-      console.log(calculation);
-
-      /**
-       * mwst zurück ins html
+       * -prüfen ob 19% ausgewählt wurde
+       * -berechnung
+       * -daten zurück ins html
        */
 
-      document.getElementById("mwst").innerHTML = Math.round(calculation);
+      if (document.getElementById("nineteen_percent").checked) {
+        const userInput = Number(document.getElementById("userInput").value);
 
-      /**
-       * gesamtbetrag zurück ins html
-       */
-      document.getElementById("final_price").innerHTML =
-        userInput - calculation;
-    }
+        calculation = userInput / 1.19;
+
+        document.getElementById("final_price").innerHTML = Math.round(calculation) + " €";
+        document.getElementById("mwst").innerHTML = Math.round(userInput - calculation) + " €";
+
+        /**
+         * -7% wurde ausgewählt
+         * -berechnung
+         * -daten zurück ins html
+         */
+      } else {
+        const userInput = Number(document.getElementById("userInput").value);
+
+        calculation = userInput / 1.07;
+
+        document.getElementById("final_price").innerHTML = Math.round(calculation) + " €";
+        document.getElementById("mwst").innerHTML = Math.round(userInput - calculation) + " €";
+      }
   }
 }
